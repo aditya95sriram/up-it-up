a79a3496779308c45a00706ffa9ce078eeb51be4
Aditya Sriram <aditya95sriram@gmail.com>
2017-03-21 13:23:46 +0530

    Intermediate commit
    Nothing sensible to commit, just to switch branches

--- trial_sketch.js
+++ trial_sketch.js
@@ -1,6 +1,4 @@
 var sz = 200, pad = 10;
-var state, empty;
-var labels = {numbers: false, letters: false};
 var canvas;
 
 var images = {}
@@ -21,88 +19,16 @@ function setup() {
   canvas = createCanvas(windowHeight*0.75,windowHeight*0.75);
   windowResized();
   canvas.parent('sketch');
-  state = [[2,2,2],[2,0,2],[2,2,2]];
-  ids = [[1,2,3],[4,5,6],[7,8,9]];
-  empty = [1,1];
-  textSize(50);
-  textAlign(CENTER,CENTER);
-  textFont("Verdana");
-  angleMode(DEGREES);
 }
 
-function draw_iso_face() {
-  push();
-  beginShape();
-  vertex(0,0);
-  vertex(sz*cos(30),-sz*sin(30));
-  vertex(sz*cos(30),sz-sz*sin(30));
-  vertex(0,sz);
-  endShape(CLOSE);
-  pop();
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
-function find_corner(i, j) {
-  var x = width/2 + sz/2 + i*sz, y = j*sz, theta = 30;
-  var nx = x*cos(theta) - y*cos(theta),
-  ny = x*sin(theta) + y*sin(theta);
-  return {x: nx, y: ny};
+function draw(){
+  background(255);
+  box();
 }
-
-function draw_iso_cube(i, j) {
-  /*
-  switch(state[j][i]) {
-    case 1: // top
-
-      break;
-    case 2: // bottom
-
-      break;
-    case 3: // left
-
-      break;
-    case 4: // right
-
-      break;
-    case 5: // front
-
-      break;
-    case 6: // back
-
-      break;
-    default:
-
-  }*/
-  var corner = find_corner(i, j);
-  push();
-  translate(corner.x, corner.y);
-  draw_iso_face();
-  rotate(120);
-  draw_iso_face();
-  rotate(120);
-  draw_iso_face();
-  pop();
-}
-
-function draw_board() {
-  for (var j=0; j<3; j++) {
-    for (var i=0; i<3; i++) {
-      draw_iso_cube(i, j);
-    }
-  }
-}
-
-function draw() {
-  background(51);
-  push();
-  noFill();
-  stroke(255);
-  strokeWeight(3);
-
-   draw_board();
-  draw_iso_cube(0,0);
-  pop();
-}
-
 
 $('document').ready(function() {
   // initialize settings
